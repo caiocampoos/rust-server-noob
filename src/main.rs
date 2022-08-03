f9c55b33e079e77814a873772c3a9e63478e03c6
caiocampos <caiocampoosbr@gmail.com>
2022-08-03 12:06:06 -0300

    tutorial andy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
+
 fn main() {
-    println!("Hello, world!!!!!!!!suamanca");
+    let listener: TcpListener =
+        TcpListener::bind("127.0.0.1:3000").unwrap();
+    
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
