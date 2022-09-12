3ef67997ae124b32f94a09a5cc2d19bdb7c3447a
caiocampos <caiocampoosbr@gmail.com>
2022-09-12 17:40:29 -0300

    more req res stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use std::net::TcpListener;
-
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let listener: TcpListener =
-        TcpListener::bind("127.0.0.1:3000").unwrap();
-    
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-        println!("Connection established!");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 Ok\r\n\r\n";
+    
+    stream.write_all(response.as_bytes()).unwrap();
 }
